3ada3da76b813cf54be64200fab4c24505e72d32
Adam Etienne <eadam@lunasys.fr>
2010-05-05 18:59:26 +0200

    fixes

--- convertPgdl.php
+++ convertPgdl.php
@@ -1,13 +1,22 @@
 <?
 //error_reporting(E_ALL);
-
 $f = fopen("/tmp/tmp.log","w+");
 if( $f ) {
-	fwrite( $f, $_POST["data"] );
+	fwrite( $f, stripslashes( $_POST["data"] ) );
 	fclose( $f );
 }
 
-$f = popen( "perl ".getcwd()."/syslog2picviz.pl /tmp/tmp.log 2>&1", "r" );
+switch( $_POST["logtype"] ) {
+case "apache_access":
+	$cmd = "apache-access2picviz.pl /tmp/tmp.log";
+break;
+case "syslog":
+default:
+	$cmd = "syslog2picviz.pl /tmp/tmp.log";
+break;
+}
+
+$f = popen( "perl ".getcwd()."/$cmd 2>&1", "r" );
 //echo "$f " . gettype($f) . "\n";
 if( $f ) {
 	while( !feof($f) ) {
